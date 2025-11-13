443e129cc9812feb4886df6de993a1a114d71819
Danylo <parrotfromukraine@MacBook-Air-Danylo.local>
2025-11-13 20:07:06 +0100

    update

--- init.lua
+++ init.lua
@@ -2,9 +2,21 @@ if vim.loader then
 	vim.loader.enable()
 end
 
+vim.o.termguicolors = true
+
 _G.dd = function(...)
 	require("util.debug").dump(...)
 end
 vim.print = _G.dd
 
 require("config.lazy")
+
+-- установить тему (пример)
+vim.cmd([[colorscheme gruvbox]])
+
+-- затем сделать фон прозрачным
+vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- иногда также полезно:
+vim.api.nvim_set_hl(0, "SignColumn", { bg = "none" })
+vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
